4e670748ac2a087aaa8d64d788d73f678d74982f
Harry Gillen <harrygillen-tl@Harrys-MacBook-Pro.local>
2026-03-18 11:45:46 -0400

    refactor: extract all CSS to design token variables

--- packages/dashboard/src/main.tsx
+++ packages/dashboard/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './styles/variables.css';
+import './styles/reset.css';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
